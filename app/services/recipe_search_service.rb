337a1de33aba6446c0c72031c8fa2cd34f31e855
Myriam Delbreil <myriam.delbreil@live.fr>
2026-03-31 15:00:55 +0200

    fixes

--- app/services/recipe_search_service.rb
+++ app/services/recipe_search_service.rb
@@ -13,21 +13,16 @@ class RecipeSearchService
   private
 
   def find_recipes
-    # On force le left_joins pour que search_by_name et search_by_ingredients soient compatibles
     name_ids = Recipe.search_by_name(@query).pluck(:id)
     ingredient_ids = Recipe.search_by_ingredients_names(@query).pluck(:id)
 
-    # On fusionne les IDs (le .uniq enlève les doublons)
     all_ids = (name_ids + ingredient_ids).uniq
 
-    # On renvoie une relation basée sur ces IDs pour que les tris fonctionnent après
     Recipe.where(id: all_ids)
   end
 
   def fallback_to_basic_ingredients
-    # On utilise SHARED_INGREDIENTS défini dans ton modèle ou ici
-    # On reste sur une structure simple qui sera complétée par les scopes de tri
-    basic_ids = Ingredient.where("name ILIKE ANY (array[?])", Recipe::SHARED_INGREDIENTS).pluck(:id)
+    basic_ids = Ingredient.where("name ILIKE ANY (array[?])", SHARED_INGREDIENTS).pluck(:id)
     Recipe.joins(:recipe_ingredients)
           .where(recipe_ingredients: { ingredient_id: basic_ids })
           .group("recipes.id")
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "hotwire_combobox", to: "hotwire_combobox.js" # ou le chemin vers le JS de la gem
-pin "hw_combobox", to: "hw_combobox.js", preload: true # <--- INDISPENSABLE
+pin "hotwire_combobox", to: "hotwire_combobox.js"
+pin "hw_combobox", to: "hw_combobox.js", preload: true
